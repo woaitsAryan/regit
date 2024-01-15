73559e1c0dea3426883a161ce75c5d5faa8644c2
woaitsAryan <yoprince.ab@gmail.com>
2024-01-15 18:50:32 +0530

    chore: refactored code

--- regit.go
+++ regit.go
@@ -2,33 +2,11 @@ package main
 
 import (
 	"github.com/spf13/cobra"
-	"github.com/woaitsAryan/regit/tools"
+	"github.com/woaitsAryan/regit/setup"
 )
 
 func main() {
-	var ownGitCommand = &cobra.Command{
-		Use:   "own [path]",
-		Short: "Own the commits of a repository",
-		Args:  cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			path := args[0]
-			tools.Owngit(path)
-		},
-	}
-
-	var blameGitCommand = &cobra.Command{
-		Use:   "blame [path] [name] [email]",
-		Short: "Blame the commits of a repository",
-		Args:  cobra.ExactArgs(3),
-		Run: func(cmd *cobra.Command, args []string) {
-			path := args[0]
-			name := args[1]
-			email := args[2]
-			tools.Blamegit(path, name, email)
-		},
-	}
-
 	var rootCmd = &cobra.Command{Use: "regit"}
-	rootCmd.AddCommand(ownGitCommand, blameGitCommand)
+	rootCmd.AddCommand(setup.OwnGitCommand, setup.BlameGitCommand)
 	cobra.CheckErr(rootCmd.Execute())
 }
---
+++ setup/commands.go
@@ -0,0 +1,46 @@
+package setup
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/woaitsAryan/regit/tools"
+)
+
+var flags = make(map[string]bool)
+var verbose bool
+var quiet bool
+
+var OwnGitCommand *cobra.Command
+var BlameGitCommand *cobra.Command
+
+func init() {
+	OwnGitCommand = &cobra.Command{
+		Use:   "own [path]",
+		Short: "Own the commits of a repository",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			path := args[0]
+			flags["verbose"] = verbose
+			flags["quiet"] = quiet
+			tools.Owngit(path, flags)
+		},
+	}
+
+	OwnGitCommand.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
+	OwnGitCommand.PersistentFlags().BoolVarP(&quiet, "quiet", "q", false, "quiet output")
+
+	var BlameGitCommand = &cobra.Command{
+		Use:   "blame [path] [name] [email]",
+		Short: "Blame the commits of a repository",
+		Args:  cobra.ExactArgs(3),
+		Run: func(cmd *cobra.Command, args []string) {
+			path := args[0]
+			name := args[1]
+			email := args[2]
+			flags["verbose"] = verbose
+			flags["quiet"] = quiet
+			tools.Blamegit(path, name, email, flags)
+		},
+	}
+	BlameGitCommand.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
+	BlameGitCommand.PersistentFlags().BoolVarP(&quiet, "quiet", "q", false, "quiet output")
+}
