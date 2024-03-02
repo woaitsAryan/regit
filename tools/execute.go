c7eb789bceef4cb62dfa2ca5474a6cfbcda6af14
woaitsAryan <yoprince.ab@gmail.com>
2024-03-02 11:04:32 +0530

    feat: Added retime command

--- tools/execute.go
+++ tools/execute.go
@@ -4,23 +4,46 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"strconv"
+	"strings"
 )
 
 func ExecuteRewrite(source string, command []string, flags map[string]bool) {
 	bashScriptPath := "./git-filter-repo"
 
-    args := append([]string{"--source", source, "--target", source}, command...)
+	args := append([]string{"--source", source, "--target", source}, command...)
 
 	cmd := exec.Command(bashScriptPath, args...)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
-	if(flags["quiet"]){
+	if flags["quiet"] {
 		return
 	}
-	if(flags["verbose"]){
+	if flags["verbose"] {
 		fmt.Println(string(output))
 	}
 	fmt.Println("Done!")
+}
+
+func getTotalCommits(source string, flags map[string]bool) int {
+	totalCommits := fmt.Sprintf("git -C %s log --oneline | wc -l", source)
+
+	cmd := exec.Command("bash", "-c", totalCommits)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+	outputStr := string(output)
+	outputInt, err := strconv.Atoi(strings.TrimSpace(outputStr))
+	if err != nil {
+		log.Fatalf("strconv.Atoi() failed with %s\n", err)
+	}
+	if flags["quiet"] {
+		return outputInt
+	}
+	fmt.Printf("Total number of commits: %d\n", outputInt)
+
+	return outputInt
 }
