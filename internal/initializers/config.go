d4473bc98197b066f89a090619407ffa5c3e144d
woaitsAryan <yoprince.ab@gmail.com>
2024-05-14 19:20:57 +0530

    chore: changed openai model

--- internal/initializers/config.go
+++ internal/initializers/config.go
@@ -2,6 +2,6 @@ package initializers
 
 const (
 	CONFIG_FILE_NAME = "config"
-	OPENAI_MODEL = "gpt-3.5-turbo"
+	OPENAI_MODEL = "gpt-3.5-turbo-0125"
 	OPENAI_URL = "https://api.openai.com/v1/chat/completions"
 )
